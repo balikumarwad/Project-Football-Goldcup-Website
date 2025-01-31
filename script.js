cbe63bf8280b97fda4b1338b010a32a5f807b532
Sangamm Wad <balikummarwad@gmail.com>
2025-01-31 23:40:25 +0545

    Add PWA support with manifest and service worker; create About Us page
    Navbar New Made

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
-const hamburger = document.getElementById('hamburger');
-const navbar = document.getElementById('navbar');
+// const hamburger = document.getElementById('hamburger');
+// const navbar = document.getElementById('navbar');
 
-hamburger.addEventListener('click', () => {
-  // Toggle the open class on hamburger and navbar
-  hamburger.classList.toggle('open');
-  navbar.classList.toggle('open');
+// hamburger.addEventListener('click', () => {
+//   // Toggle the open class on hamburger and navbar
+//   hamburger.classList.toggle('open');
+//   navbar.classList.toggle('open');
 
-  // Toggle visibility of icons
-  const barsIcon = hamburger.querySelector('.fa-bars');
-  const timesIcon = hamburger.querySelector('.fa-times');
+//   // Toggle visibility of icons
+//   const barsIcon = hamburger.querySelector('.fa-bars');
+//   const timesIcon = hamburger.querySelector('.fa-times');
 
-  if (navbar.classList.contains('open')) {
-    barsIcon.style.display = 'none';
-    timesIcon.style.display = 'inline';
-  } else {
-    barsIcon.style.display = 'inline';
-    timesIcon.style.display = 'none';
-  }
-});
+//   if (navbar.classList.contains('open')) {
+//     barsIcon.style.display = 'none';
+//     timesIcon.style.display = 'inline';
+//   } else {
+//     barsIcon.style.display = 'inline';
+//     timesIcon.style.display = 'none';
+//   }
+// });
+
+function toggleNavbar() {
+  var navbar = document.getElementById("navbar");
+  var hamburger = document.getElementById("hamburger");
+  navbar.classList.toggle("open");
+  hamburger.classList.toggle("open");
+
+  // Toggle the hamburger to cross icon
+  // if (navbar.classList.contains("responsive")) {
+  //     icon.classList.remove("fa-bars");
+  //     icon.classList.add("fa-times"); // Change to cross icon
+  // } else {
+  //     icon.classList.remove("fa-times");
+  //     icon.classList.add("fa-bars"); // Change to hamburger icon
+  // }
+}
 
---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "pwa-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/script.js",
+  "/logo.jpg",
+  "logo.jpg"
+];
+
+// Install the service worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch cached resources
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Update cache
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
